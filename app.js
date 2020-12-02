f1413bf22f0ec50867e8e210cbf4d74d9f89d717
abdelwahed <38771074+yurrey@users.noreply.github.com>
2020-12-02 14:32:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('body,html').animate({
-      scrollTop: $(hash).offset().top
-      }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
-});
 
-var width = $(window).width(); 
+const header = document.querySelector('#header');
+const me = document.querySelector('#me');
+const explore = document.querySelector('#explore')
 
-window.onscroll = function(){
-if ((width >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","100% auto");        
+
+
+
+explore.addEventListener('click', ()=>{
+    me.scrollIntoView({behavior:'smooth'})
+})
+
+window.addEventListener('scroll', ()=>{
+    if (window.pageYOffset > header.offsetTop){
+        header.classList.add('sticky')
+       console.log(header.classList)
+    } else {
+        console.log('f')
     }
-}
-};
+})
 
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1450);
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 1200, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  
+  var width = $(window).width(); 
+  window.onscroll = function(){
+  if ((width >= 900)){
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+          $("#middle").css("background-size","150% auto");
+      }else{
+          $("#middle").css("background-size","100% auto");        
+      }
+  }
+  };
+  
+  setTimeout(function(){
+      $("#loading").addClass("animated fadeOut");
+      setTimeout(function(){
+        $("#loading").removeClass("animated fadeOut");
+        $("#loading").css("display","none");
+      },800);
+  },1450);
